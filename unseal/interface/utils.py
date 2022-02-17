a8573f7f7266a03bb9aa7f13657795b2309338fc
Tom Frederik <tlieberum@outlook.de>
2022-02-17 17:20:02 +0100

    adding sampling and moving functions

--- unseal/interface/utils.py
+++ unseal/interface/utils.py
@@ -2,10 +2,12 @@ import importlib
 import json
 from typing import List, Tuple, Optional
 
+import einops
+import pysvelte as ps
 import streamlit as st
-import torch
 
 from unseal.hooks import HookedModel
+from unseal.hooks.common_hooks import gpt_get_attention_hook
 from unseal.transformers_util import load_from_pretrained, get_num_layers
 
 def init_session_state(variables: List[str]) -> None:
@@ -32,9 +34,17 @@ def on_config_submit(model_name: str) -> Tuple:
     model.to(st.session_state.device).eval()
     
     st.session_state.num_layers = get_num_layers(model)
-    print(st.session_state.num_layers)
 
     return model, tokenizer, config
+
+def on_sampling_config_change():
+    st.session_state.sample_kwargs = dict(
+        temperature=st.session_state.temperature,
+        max_length=st.session_state.response_length,
+        top_p=st.session_state.top_p,
+        repetition_penalty=1/st.session_state.repetition_penalty,
+        num_beams=st.session_state.num_beams
+    )
 
 @st.experimental_singleton
 def load_model(model_name: str) -> Tuple:
@@ -94,3 +104,59 @@ def startup(variables: List[str], mode_file_path: Optional[str] = './registered_
 
     # load externally registered models
     load_registered_models(mode_file_path)
+
+def sample_text(storage_key, label, key):
+    text = st.session_state[key]
+    if st.session_state.prefix_prompt is not None and len(st.session_state.prefix_prompt) > 0:
+        text = st.session_state.prefix_prompt + '\n' + text
+    model_inputs = st.session_state.tokenizer.encode(text, return_tensors='pt').to(st.session_state.device)
+    output = st.session_state.model.model.generate(model_inputs, **st.session_state.sample_kwargs, min_length=0, output_attentions=True)
+    output_text = st.session_state.tokenizer.decode(output[0], skip_special_tokens=True)
+    if st.session_state.prefix_prompt is not None and len(st.session_state.prefix_prompt) > 0:
+        output_text = output_text.lstrip(st.session_state.prefix_prompt + '\n')
+    
+    st.session_state[storage_key] = output_text
+    text_change()
+
+def create_sample_sliders():
+    st.slider(label="Temperature", min_value=0., max_value=1.0, value=0., step=0.01, key='temperature', on_change=on_sampling_config_change)
+    st.slider(label="Response length", min_value=64, max_value=1024, value=64, step=1, key='response_length', on_change=on_sampling_config_change)
+    st.slider(label="Top P", min_value=0., max_value=1.0, value=1., step=0.01, key='top_p', on_change=on_sampling_config_change)
+    st.slider(label="Repetition Penalty (1 = no penalty)", min_value=0.01, max_value=1.0, value=1., step=0.01, key='repetition_penalty', on_change=on_sampling_config_change)
+    st.slider(label="Number of Beams", min_value=1, max_value=10, value=1, step=1, key='num_beams', on_change=on_sampling_config_change)
+
+def on_text_change(storage_key, text_key):
+    st.session_state[storage_key] = st.session_state[text_key]
+    text_change()
+    
+def text_change():
+    cols = st.columns(2)
+    
+    for k, text in enumerate([st.session_state.storage_1, st.session_state.storage_2]):
+        with cols[k]:
+            if st.session_state.prefix_prompt is not None and len(st.session_state.prefix_prompt) > 0:
+                text = st.session_state.prefix_prompt + '\n' + text
+            # if st.session_state.suffix_prompt is not None and len(st.session_state.suffix_prompt) > 0:
+            #     text = text + '\n' + st.session_state.suffix_prompt
+
+            if text is None or len(text) == 0:
+                return
+            if st.session_state.model_name.endswith('grokking'):
+                raise NotImplementedError
+            else:
+                tokenized_text = st.session_state.tokenizer.tokenize(text)
+                tokenized_text = [token.replace("Ġ", " ") for token in tokenized_text]
+                tokenized_text = [token.replace("Ċ", "\n") for token in tokenized_text]
+                model_input = st.session_state.tokenizer.encode(text, return_tensors='pt').to(st.session_state.device)
+
+                layer_hooks = [gpt_get_attention_hook(i, f'layer_{i}') for i in range(st.session_state.num_layers)]
+                st.session_state.model.forward(model_input, hooks=layer_hooks, output_attentions=True)
+                layer_attentions = [st.session_state.model.save_ctx[f'layer_{i}']['attn'] for i in range(st.session_state.num_layers)]
+                layer_attentions = [einops.rearrange(attn[0], 'h n1 n2 -> n1 n2 h') for attn in layer_attentions]
+            
+            for i, attn in enumerate(layer_attentions):
+                html_object = ps.AttentionMulti(tokens=tokenized_text, attention=attn, head_labels=[f'{i}:{j}' for j in range(attn.shape[-1])])
+                html_object = html_object.update_meta(suppress_title=True)
+                html_str = html_object.html_page_str()
+                with st.expander(f'Layer {i}'):
+                    st.components.v1.html(html_str, height=600)
