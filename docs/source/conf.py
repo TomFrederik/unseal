84537a8ae2c387e819541dc11f32695ebef4f389
Tom Frederik <tlieberum@outlook.de>
2022-03-12 12:31:05 +0100

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Tom Lieberum'
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
