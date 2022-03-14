849587b894dd140566b6abc95eeedcf619d11554
Tom Frederik <tlieberum@outlook.de>
2022-03-14 15:30:33 +0100

    add type hints

--- unseal/hooks/rome_hooks.py
+++ unseal/hooks/rome_hooks.py
@@ -2,7 +2,7 @@
 # from the ROME paper: https://openreview.net/forum?id=mMECu_poAs
 
 # Some hooks that I've only used in the context of the ROME paper so far --> possibly migrate this to the unseal_experiments repo
-from typing import Callable
+from typing import Callable, Optional
 
 import torch
 
@@ -10,7 +10,11 @@ from . import util
 from . import common_hooks
 
 
-def additive_output_noise(indices, mean=0, std=0.1):
+def additive_output_noise(
+    indices: str, 
+    mean: Optional[float] = 0, 
+    std: Optional[float] = 0.1
+) -> Callable:
     slice_ = util.create_slice_from_str(indices)
     def func(save_ctx, input, output):
         noise = mean + std * torch.randn_like(output[slice_])
@@ -18,7 +22,10 @@ def additive_output_noise(indices, mean=0, std=0.1):
         return output
     return func
 
-def hidden_patch_hook_fn(position, replacement_tensor):
+def hidden_patch_hook_fn(
+    position: int, 
+    replacement_tensor: torch.Tensor,
+) -> Callable:
     indices = "...," + str(position) + len(replacement_tensor.shape) * ",:"
     inner = common_hooks.replace_activation(indices, replacement_tensor)    
     def func(save_ctx, input, output):
