116d65f58278d24a556cf7f80e3e334119ce84f7
Tom Frederik <tlieberum@outlook.de>
2022-03-16 09:37:23 +0100

    fix import

--- unseal/hooks/rome_hooks.py
+++ unseal/hooks/rome_hooks.py
@@ -1,7 +1,7 @@
 #TODO for version 1.0.0: Remove this file
 import logging
 
-from common_hooks import additive_output_noise, hidden_patch_hook_fn
+from .common_hooks import additive_output_noise, hidden_patch_hook_fn
 
 logging.warning("rome_hooks.py is deprecated and will be removed in version 1.0.0. Please use common_hooks.py instead.")
 
