9ed44fe4f5aafa558064c0f1e2f926a83604f1a2
Tom Frederik <tlieberum@outlook.de>
2022-05-01 16:45:22 +0100

    update tests

--- tests/hooks/test_util.py
+++ tests/hooks/test_util.py
@@ -1,10 +1,12 @@
 import logging
 from typing import Iterable
 
-from unseal.hooks import util
 import numpy as np
 import pytest
 import torch
+from unseal.hooks import util
+
+
 def test_create_slice_from_str():
     arr = np.random.rand(10,10,10)
     valid_idx_strings = [
@@ -29,18 +31,21 @@ def test_create_slice_from_str():
         with pytest.raises(ValueError):
             subarry = arr[util.create_slice_from_str(idx)]
 
+####
+
 def test_recursive_to_device():
     if not torch.cuda.is_available():
         logging.warning('CUDA not available, skipping recursive_to_device test.')
         return
     
-    tensor = torch.rand(10,10,10)
+    tensor = torch.rand(10,10)
     tensor = util.recursive_to_device(tensor, torch.device('cuda'))
-    assert tensor.device.type == 'cuda'
+    nested_list = [tensor, [[tensor], tensor]]
+    nested_tuple = (tensor, [(tensor,), tensor])
     
-    iterable = [tensor, [[tensor], tensor]]
-    
-    recursive_cuda_check(iterable)
+    recursive_cuda_check(tensor)
+    recursive_cuda_check(nested_list)
+    recursive_cuda_check(nested_tuple)    
 
 def recursive_cuda_check(iterable):
     if isinstance(iterable, torch.Tensor):
@@ -48,3 +53,27 @@ def recursive_cuda_check(iterable):
     elif isinstance(iterable, Iterable):
         for item in iterable:
             recursive_cuda_check(item)
+
+#####
+
+def test_recursive_detach():
+    undetach_tensor = torch.rand(10,10, requires_grad=True)
+    tensor = util.recursive_detach(undetach_tensor)
+    nested_list = [tensor, [[tensor], tensor]]
+    nested_tuple = (tensor, [(tensor,), tensor])
+    empty_list = []
+    empty_tuple = tuple([])
+    
+    assert undetach_tensor.requires_grad
+    recursive_detach_check(tensor)
+    recursive_detach_check(nested_list)
+    recursive_detach_check(nested_tuple)    
+    recursive_detach_check(empty_list)    
+    recursive_detach_check(empty_tuple)    
+
+def recursive_detach_check(iterable):
+    if isinstance(iterable, torch.Tensor):
+        assert not iterable.requires_grad
+    elif isinstance(iterable, Iterable):
+        for item in iterable:
+            recursive_detach_check(item)
