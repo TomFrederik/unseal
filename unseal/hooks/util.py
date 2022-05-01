8fd714aa88afb27303bd441a66428fb8f6548a02
Tom Frederik <tlieberum@outlook.de>
2022-05-01 16:45:11 +0100

    add recursive_detach and update docs and type annotation

--- unseal/hooks/util.py
+++ unseal/hooks/util.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from typing import Iterable, Tuple, Union
+from typing import Iterable, Tuple, Union, List
 
 import numpy as np
 import torch
@@ -21,28 +21,46 @@ def create_slice_from_str(indices: str) -> slice:
 def recursive_to_device(
     iterable: Union[Iterable, torch.Tensor], 
     device: Union[str, torch.device],
-) -> Iterable:
+) -> Union[List, Tuple, torch.Tensor]:
     """Recursively puts an Iterable of (Iterable of (...)) tensors on the given device
+    This creates a copy of the iterable in question, which can be very costly.
 
     :param iterable: Tensor or Iterable of tensors or iterables of ...
     :type iterable: Tensor or Iterable
     :param device: Device on which to put the object
     :type device: Union[str, torch.device]
-    :raises TypeError: Unexpected tyes
-    :return: Nested iterable with the tensors on the new device
-    :rtype: Iterable
+    :raises TypeError: Unsupported type.
+    :return: Nested iterable with the tensors on the new device. Default type is List, unless iterable is a tuple in which case a tuple is returned.
+    :rtype: Union[List, Tuple, torch.Tensor]
     """
     if isinstance(iterable, torch.Tensor):
         return iterable.to(device)
+    elif isinstance(iterable, Iterable):
+        out = [recursive_to_device(o, device) for o in iterable]
+        if isinstance(iterable, Tuple):
+            out = tuple(out)
+        return out
+    else:
+        raise TypeError(f"Can not handle object of type {type(iterable)}.")
 
-    new = []
-    for i, item in enumerate(iterable):
-        if isinstance(item, torch.Tensor):
-            new.append(item.to(device))
-        elif isinstance(item, Iterable):
-            new.append(recursive_to_device(item, device))
-        else:
-            raise TypeError(f'Expected type tensor or Iterable but got {type(item)}.')
-    if isinstance(iterable, Tuple):
-        new = tuple(new)
-    return new
+def recursive_detach(
+    iterable: Union[torch.Tensor, Iterable]
+) -> Union[torch.Tensor, List, Tuple]:
+    """Recursively detaches an Iterable of (Iterable of (...)) tensors.
+    This creates a copy of the iterable in question, which can be very costly.
+
+    :param iterable: Tensor or Iterable of tensors or iterables of ...
+    :type iterable: Union[torch.Tensor, Iterable]
+    :raises TypeError: Unsupported type.
+    :return: Nested iterable with the tensors detached. Default type is List, unless iterable is a tuple in which case a tuple is returned.
+    :rtype: Union[torch.Tensor, List, Tuple]
+    """
+    if isinstance(iterable, torch.Tensor):
+        return iterable.detach()
+    elif isinstance(iterable, Iterable):
+        out = [recursive_detach(o) for o in iterable]
+        if isinstance(iterable, tuple):
+            out = tuple(out)
+        return out
+    else:
+        raise TypeError(f"Can not detach object of type {type(iterable)}.")
